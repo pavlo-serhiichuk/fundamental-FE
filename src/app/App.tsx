684e001b855b087eb486fbc9c9a4c005f74c79d4
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-02-08 10:09:24 +0200

    12 lesson. added first Widget + added first UI Kit component(with themes)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,18 @@ import {Theme} from './providers/ThemeProvider/lib/ThemeContext'
 import {classNames} from 'shared/lib/classNames/classNames'
 import {useTheme} from './providers/ThemeProvider'
 import {AppRouter} from 'app/providers/router'
+import {Navbar} from 'widgets/Navbar'
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
-    <div className={classNames('app', {hovered: true, }, [theme])}>
-      <div style={{display: 'inline-flex'}}>
-        <Link to='/'>Main</Link>
-        <Link to='/about'>About</Link>
-        <button onClick={toggleTheme}>{theme === Theme.LIGHT ? Theme.DARK: Theme.LIGHT}</button>
+    <div className={classNames('app', {hovered: true,}, [theme])}>
+      <div>
+        <Navbar />
       </div>
       <AppRouter />
+      <button onClick={toggleTheme}>{theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT}</button>
     </div>
   );
 };
