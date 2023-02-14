506c0a135a53983f9e9eb87b712afa40cc32f428
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-02-14 20:17:49 +0200

    23 lesson. added bundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HTMLWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import {BuildOptions} from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
 
 export function buildPlugins({path, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -16,7 +17,9 @@ export function buildPlugins({path, isDev}: BuildOptions): webpack.WebpackPlugin
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
     }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    }),
     new webpack.HotModuleReplacementPlugin( )
-
   ]
 }
