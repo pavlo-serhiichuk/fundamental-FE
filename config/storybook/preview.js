e900bb1f5a1efe9ce13e78c86ad28873ce0fcea4
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-02-26 13:07:41 +0200

    25 lesson. added storybook(config files)

---
+++ config/storybook/preview.js
@@ -0,0 +1,21 @@
+import {addDecorator} from "@storybook/react";
+import {StyleDecorator} from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
+import {ThemeDecorator} from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import {RouteDecorator} from "../../src/shared/config/storybook/RouteDecorator/RouteDecorator";
+import {Theme} from "../../src/app/providers/ThemeProvider";
+import {SuspenseDecorator} from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+addDecorator(StyleDecorator)
+addDecorator(ThemeDecorator(Theme.LIGHT))
+addDecorator(RouteDecorator)
+// addDecorator(SuspenseDecorator)
