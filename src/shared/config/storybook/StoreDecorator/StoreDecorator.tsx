b6cee8b6578a2650390009a679c34f7fb877a71e
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-03-27 23:57:39 +0300

    to go through pipeline part 2

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,7 +1,7 @@
 import {Story} from '@storybook/react'
-import {StateSchema, StoreProvider} from 'app/providers/StoreProvider'
 import {DeepPartial, ReducersMapObject} from '@reduxjs/toolkit'
 import {loginReducer} from 'features/AuthByUsername/model/slice/loginSlice'
+import {StateSchema, StoreProvider} from 'app/providers/StoreProvider'
 
 const defAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
   loginForm: loginReducer
