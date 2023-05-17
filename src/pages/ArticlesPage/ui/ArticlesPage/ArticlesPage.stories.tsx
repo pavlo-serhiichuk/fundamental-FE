3a55de3383b87bb0d970a6e25daebb82b3278b76
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-05-17 08:37:39 +0300

    55 lesson. added storybook screenshots

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 import ArticlesPage from './ArticlesPage';
+import {StoreDecorator} from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 export default {
   title: 'pages/ArticlesPage',
@@ -16,3 +17,4 @@ const Template: ComponentStory<typeof ArticlesPage> = () => <ArticlesPage />;
 export const Primary = Template.bind({});
 
 Primary.args = {};
+Primary.decorators = [StoreDecorator({})];
