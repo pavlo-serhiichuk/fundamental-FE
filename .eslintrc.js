6080c99adb509a1971c1e27ceb2cfa804a808f23
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-06-07 14:14:20 +0300

    added custom eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,42 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    jest: true
-  },
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:i18next/recommended", "plugin:storybook/recommended"],
-  overrides: [{
-    files: ['**/src/**/*.test.{ts, tsx}'],
-    rules: {
-      'i18next/no-literal-string': 'off'
-    }
-  }],
-  globals: {
-    __API__: true,
-    __IS_DEV__: true,
-    __PROJECT__: true,
-  },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "plugins": ["react", "@typescript-eslint", 'i18next',  "react-hooks"],
-  "rules": {
-    "@typescript-eslint/ban-ts-comment": "off",
-    'i18next/no-literal-string': ['error', {
-      markupOnly: true
+    "env": {
+        "browser": true,
+        "es2021": true,
+        jest: true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:i18next/recommended", "plugin:storybook/recommended"],
+    overrides: [{
+        files: ['**/src/**/*.test.{ts, tsx}'],
+        rules: {
+            'i18next/no-literal-string': 'off'
+        }
     }],
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "error",// Checks effect dependencies
-    "no-undef": 'off',
-    "arrow-body-style": "off"
-  }
+    globals: {
+        __API__: true,
+        __IS_DEV__: true,
+        __PROJECT__: true,
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        'i18next',
+        "react-hooks",
+        "by-pavlo-plugin"
+    ],
+    "rules": {
+        "@typescript-eslint/ban-ts-comment": "off",
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true
+        }],
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "error",// Checks effect dependencies
+        "no-undef": 'off',
+        "arrow-body-style": "off",
+        "by-pavlo-plugin/path-checker": 'error'
+    }
 };
