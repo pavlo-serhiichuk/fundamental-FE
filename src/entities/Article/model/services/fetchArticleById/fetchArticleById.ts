79001963ebe4f148da7093035b326e2aa716a2ee
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-06-08 13:34:19 +0300

    fixed import part 3

--- src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.ts
@@ -1,6 +1,6 @@
 import {createAsyncThunk} from '@reduxjs/toolkit'
 import {ThunkConfig} from 'app/providers/StoreProvider'
-import {Article} from '../../types/article'
+import {Article} from 'entities/Article'
 
 
 // те, що отримає в результаті зіпиту | те, що відправляє | якщо помилка
