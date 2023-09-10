89fc6ef662347e53231f8896103eef9106b485ea
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2023-09-10 20:50:16 +0300

    refactored entities/features

--- src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/features/ArticleSortSelector/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -4,7 +4,7 @@ import {useTranslation} from 'react-i18next'
 import cls from './ArticleSortSelector.module.scss'
 import {Select, SelectOption} from '@/shared/ui/Select'
 import {SortOrder} from '@/shared/types'
-import {ArticleSortField} from '../../model/consts/consts'
+import {ArticleSortField} from '@/entities/Article/model/consts/consts'
 
 interface ArticleSortSelectorProps {
   className?: string;
